359711763994696c534fccd3cb88a88bdcd473ae
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-17 22:32:04 +0530

    Complete UserPost page

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import UserHeader from '../components/UserHeader'
+import UserPost from '../components/UserPost'
 
 const UserPage = () => {
   return (
+    <>
 <UserHeader/>
+<UserPost/>
+</>
   )
 }
 
