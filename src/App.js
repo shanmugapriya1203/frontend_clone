d117d85501b8349eda4cd7868c5c7024883a832c
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-24 22:02:55 +0530

    create HomePage with button

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Routes,Route } from 'react-router-dom';
 import UserPage from './pages/UserPage';
 import PostPage from './pages/PostPage';
 import Header from './components/Header';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
 <Container maxW="620px">
   <Header/>
  <Routes>
+  <Route path='/' element={<HomePage/>}/>
 <Route path='/:username' element={<UserPage/>}/>
 <Route path='/:username/post/:pid' element={<PostPage/>}/>
  </Routes>
