858a832e11f59e21bf0c2c1160f32b46ac9b8cac
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-31 19:55:45 +0530

    Link for home page and profile page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,38 @@
-import { Flex, Image ,useColorMode} from '@chakra-ui/react'
+import { Flex, Image ,Link,useColorMode} from '@chakra-ui/react'
 import React from 'react'
+import { useRecoilValue } from 'recoil'
+import userAtom from '../atoms/userAtom'
+import {AiFillHome} from 'react-icons/ai'
+import { RxAvatar} from 'react-icons/rx'
+import { Link as RouterLink } from 'react-router-dom'
 
 const Header = () => {
     const {colorMode,toggleColorMode}=useColorMode()
+    const user=useRecoilValue(userAtom)
   return (
-  <Flex justifyContent={"center"} mt={6} mb={12}>
+  <Flex justifyContent={"space-between"} mt={6} mb={12}>
+{
+  user && (
+    <Link as={RouterLink} to="/">
+   <AiFillHome size={24}/>
+    </Link>
+  )
+}
     <Image cursor={"pointer"}
     alt='logo'
     w={6}
     src={colorMode ==='dark'? "/light-logo.svg" :'/dark-logo.svg'}
     onClick={toggleColorMode}
-    /></Flex>
+    />
+    {
+  user && (
+    <Link as={RouterLink} to={`/${user.username}`}>
+   <RxAvatar size={24}/>
+    </Link>
+  )
+}
+    
+    </Flex>
   )
 }
 
